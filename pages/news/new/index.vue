dc662ecbdc3fcd7fb4ffc6a8bc54623dc1db0d41
lexdier <saviolexdier@gmail.com>
2023-07-14 14:04:27 -0300

    feat: create new news form

---
+++ pages/news/new/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <BForm>
+    <BCard header="Create your news">
+      <BRow align-v="center" class="mb-4 g-4">
+        <BCol lg="6">
+          <BFormGroup label="Title">
+            <BFormInput v-model="news.title" type="text"/>
+          </BFormGroup>
+        </BCol>
+        <BCol lg="6">
+          <BFormGroup label="Author">
+            <BFormInput v-model="news.author" type="text"/>
+          </BFormGroup>
+        </BCol>
+        <BCol lg="6">
+          <BFormGroup label="Link">
+            <BFormInput v-model="news.url" type="url"/>
+          </BFormGroup>
+        </BCol>
+        <BCol lg="12">
+          <BFormGroup label="Description">
+            <BFormTextarea v-model="news.description" type="text"/>
+          </BFormGroup>
+        </BCol>
+      </BRow>
+
+      <template #footer>
+        <BRow align-h="end">
+          <BCol cols="auto">
+            <BBtn variant="danger" @click="$router.back()">
+              <span>Cancel</span>
+            </BBtn>
+          </BCol>
+          <BCol cols="auto">
+            <BBtn variant="primary" @click="createNews">
+              <span>Save</span>
+            </BBtn>
+          </BCol>
+        </BRow>
+      </template>
+    </BCard>
+  </BForm>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component({})
+export default class NewNews extends Vue {
+
+  protected news = {
+    title: '',
+    url: '',
+    description: '',
+    author: '',
+  }
+
+  public createNews() {
+    console.log(this.news)
+  }
+
+}
+</script>
