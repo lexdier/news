6a871d59136887c3ff5d488781168eafac17c91d
lexdier <saviolexdier@gmail.com>
2023-07-15 19:03:18 -0300

    refactor: change to dynamic page id

---
+++ pages/news/edit/_id/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <BOverlay :show="loading">
+    <NewsForm v-model="news" @handleNews="update"/>
+  </BOverlay>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'nuxt-property-decorator'
+import NewsForm from "../../../../components/NewsForm.vue";
+
+@Component({components: {NewsForm}})
+export default class EditNews extends Vue {
+
+  public id = ""
+  public loading = false
+  protected news = {}
+
+  mounted() {
+    this.id = this.$route.params.id
+
+    this.$axios.$get(`news/${this.id}`)
+      .then((news) => {
+        this.news = news
+      })
+  }
+
+  public update() {
+    this.loading = true
+
+    this.$axios.put(`/news/${this.id}`, this.news)
+      .then(() => {
+        this.loading = false
+        this.$router.back()
+      })
+  }
+}
+</script>
