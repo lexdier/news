0a68935f464483458007766ea370ab09e36ecfdb
lexdier <saviolexdier@gmail.com>
2023-07-15 15:32:31 -0300

    refactor: set sidebar sticky top

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -1,14 +1,18 @@
 <template>
-  <BListGroup class="scrollspy">
-    <template v-for="(item, index) in items">
-      <BLink :key="index" router-tag="li" :to="item.to">
-        <a>
-          <FontAwesome :name="item.icon" mode="thin" class="pe-3"/>
-          <span v-text="item.title"/>
-        </a>
-      </BLink>
-    </template>
-  </BListGroup>
+  <BCard class="sticky-top" no-body>
+    <BCardBody>
+      <BListGroup class="scrollspy">
+        <template v-for="(item, index) in items">
+          <BLink :key="index" router-tag="li" :to="item.to">
+            <a>
+              <FontAwesome :name="item.icon" mode="thin" class="pe-3"/>
+              <span v-text="item.title"/>
+            </a>
+          </BLink>
+        </template>
+      </BListGroup>
+    </BCardBody>
+  </BCard>
 </template>
 
 <script lang="ts">
