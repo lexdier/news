d9a66f0b7cd7ea3ff89dfaaed006cface928b834
lexdier <saviolexdier@gmail.com>
2023-07-15 20:45:00 -0300

    refactor: remove unused sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,7 @@
   <main>
     <TheHeader/>
     <BContainer class="my-5">
-      <BRow class="g-4">
-        <BCol md="4" xl="3">
-          <TheSidebar/>
-        </BCol>
+      <BRow>
         <BCol class="d-flex justify-content-center align-items-center">
           <Nuxt/>
         </BCol>
@@ -17,9 +14,8 @@
 <script lang="ts">
 import {Component, Vue} from 'nuxt-property-decorator'
 import TheHeader from "../components/TheHeader.vue";
-import TheSidebar from "../components/TheSidebar.vue";
 
-@Component({components: {TheHeader, TheSidebar}})
+@Component({components: {TheHeader}})
 export default class Default extends Vue {
 
 }
