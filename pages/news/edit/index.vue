63763a4fa412edfcbbc1ec48569a37bef553cbb0
lexdier <saviolexdier@gmail.com>
2023-07-15 15:31:39 -0300

    refactor: display news from req to db

---
+++ pages/news/edit/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'nuxt-property-decorator'
+
+@Component({components: {}})
+export default class EditNews extends Vue {
+
+}
+</script>
